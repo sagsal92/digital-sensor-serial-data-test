bcbb4fd46c24de574d693c0cea6e7789b01a9181
Sagar salunkhe <sagsal92@gmail.com>
2018-02-26 22:40:46 +0530

    Add files via upload

---
+++ digitalInputonserial.ino
@@ -0,0 +1,15 @@
+int sensorPin = 2;
+int sensorValue = 0;
+
+void setup() 
+{
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  sensorValue = digitalRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(100);
+}
